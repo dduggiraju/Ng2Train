b9f6877eb37f3f8bda87747fda68f32b7ad07cc1
Dheeraj Duggiraju <dduggira@bechtel.com>
2017-02-13 15:31:49 -0600

    Initial set up for angular app

---
+++ SportsStore/app/app.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app',
+    template: `<div class="bg-success p-a-1 text-xs-center">
+    This is Sports Store
+    </div>`
+})
+export class AppComponent { }
