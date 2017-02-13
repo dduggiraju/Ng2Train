a498cc90b192e4cd1258cff8b29c3dba984d720b
Dheeraj Duggiraju <dduggira@bechtel.com>
2017-02-13 16:02:58 -0600

    added class

--- SportsStore/app/model/product.repository.ts
+++ SportsStore/app/model/product.repository.ts
@@ -1,3 +1,9 @@
 import { Injectable } from '@angular/core';
 import { Product } from './product.model';
 import { StaticDataSource } from './static.datasource';
+
+@Injectable()
+
+export class ProductRepository {
+    
+}
